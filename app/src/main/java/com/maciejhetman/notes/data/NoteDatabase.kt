be8d642fe454e979dad6af8bc5794cfea6759ed2
Maciek-Hetman <maciekhetman577@gmail.com>
2026-07-17 22:45:55 +0200

    bump gradle version

--- app/src/main/java/com/maciejhetman/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/maciejhetman/notes/data/NoteDatabase.kt
@@ -16,7 +16,7 @@ abstract class NoteDatabase : RoomDatabase() {
         fun getDatabase(context: Context): NoteDatabase {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, NoteDatabase::class.java, "note_database")
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigration(dropAllTables = true)
                     .build()
                     .also { Instance = it }
             }
